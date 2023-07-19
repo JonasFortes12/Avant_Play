1d27549403dc43bba866aa88912ef7b2b404d632
JonasFortes12 <jonascforte@hotmail.com>
2023-07-19 11:20:14 -0300

    Create user implemented

--- avant-play/server/controllers/UsersControllers.js
+++ avant-play/server/controllers/UsersControllers.js
@@ -1,6 +1,7 @@
-//import Prisma
+import { PrismaClient } from "@prisma/client";
 //import Zod
 
+const prisma = new PrismaClient();
 class UsersController {
 
     static listUsers = async(req, res) => {
@@ -12,7 +13,22 @@ class UsersController {
     }
 
     static createUser = async (req, res) =>{
-        res.status(200).send("Cadastrar novo usuário!")
+        const {name, email, password} = req.body;
+
+        try {
+            await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password
+                }
+            });
+            
+            res.status(200).send("Novo usuário cadastrado!")
+        } catch (err) {
+            res.status(500).json({ message: 'Erro ao cadastrar usuário', error: err.message });
+        }
+
     }
 
     static updateUser = async (req, res) =>{
