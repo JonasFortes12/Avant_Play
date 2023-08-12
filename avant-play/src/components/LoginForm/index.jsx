705c6e8dfd05b8e0b71a0931215455827808e541
JonasFortes12 <jonascforte@hotmail.com>
2023-08-12 09:33:21 -0300

    Add hooks to form filds

--- avant-play/src/components/LoginForm/index.jsx
+++ avant-play/src/components/LoginForm/index.jsx
@@ -3,7 +3,14 @@ import style from "./LoginForm.module.scss";
 import logo from "../../assets/logo-gamificacao.png";
 import Field from "../Field";
 
+import { useState } from "react";
+
 function LoginForm() {
+
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={style.form}>
       <div className={style.form__img}>
@@ -16,10 +23,24 @@ function LoginForm() {
         <br /> <span>a proteger seus dados.</span>
       </p>
 
-      <Field placeholder={"E-mail"} />
-      <Field placeholder={"Senha"} />
+      <Field 
+      value={email} 
+      placeholder={"E-mail"} 
+      onChange={(event) =>
+        setEmail(event.target.value)
+      } 
+      />
 
-      <Button value="Entrar" width="272px"/>
+      <Field 
+      type="password"
+      value={password} 
+      placeholder={"Senha"}
+      onChange={(event) =>
+        setPassword(event.target.value)
+      } 
+      />
+
+      <Button value="Entrar" width="272px" />
 
       <div className={style.form__keepConected}>
         <input type="checkbox" name="newsletter" value="subscribe"></input>
@@ -28,15 +49,13 @@ function LoginForm() {
 
       <div className={style.form__belowButtons}>
         <Button value="Registrar-se" width="111px" bgcolor="#FF691A" />
-        <Button value="Esqueci a Senha" width="141px"/>
+        <Button value="Esqueci a Senha" width="141px" />
       </div>
 
       <div className={style.form__areYouManager}>
         <label>Você é um gestor?</label>
         <Button value="Entrar como gestor" width="272px" bgcolor="#FF691A" />
       </div>
-
-
     </div>
   );
 }
